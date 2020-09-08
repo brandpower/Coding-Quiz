43fc8867152038a46534c5f9aeaf1599dad8446e
brandpower <67851117+brandpower@users.noreply.github.com>
2020-09-09 09:55:55 +1000

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+// These are objects within an array called questions - questions[0].title, questions[1].choices
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "parentheses", "curly brackets",  "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store __________",
+      choices: ["booleans", "numbers and strings", "other arrays", "all of the above"],
+      answer: "other arrays"
+    },
+    {
+      title: "String values must be enclosed within __________ when being assigned to variables.",
+      choices: ["commas", "curly braces", "quotes", "parenthesis"],
+      answer: "quotes"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the degugger is:",
+      choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+      answer: "console.log"
+    }
+    ///etc.
+  ];
+
